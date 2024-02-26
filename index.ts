c7e9813f856851cb20136e50272c6e9f9f4971ac
CyberL1 <mateusz.hf18@gmail.com>
2024-02-26 19:28:36 +0100

    Add playlist generating

--- index.ts
+++ index.ts
@@ -3,12 +3,15 @@ const env = await load({ allowEmptyValues: true });
 
 import { buildURL } from "@/utils/functions.ts";
 import { ENDPOINTS } from "@/utils/constants.ts";
-import { Channel } from "@/types.ts";
+import { Channel, Stream } from "@/types.ts";
+import { existsSync } from "@/deps.ts";
 
 if (!env.MEGOGO_LANG) {
   console.log("No MEGOGO_LANG specified, defaulting to en");
   env.MEGOGO_LANG = "en";
 }
+
+if (!existsSync("generated")) Deno.mkdirSync("generated");
 
 const params: Record<string, string> = {
   lang: env.MEGOGO_LANG,
@@ -19,6 +22,24 @@ const channelsResponse = await (await fetch(channelsUrl)).json();
 
 const channels: Channel[] = channelsResponse.data.channels;
 
+let playlist = "#EXTM3U\n";
+
 for (const [index, channel] of channels.entries()) {
-  console.log(index + 1, channel.title);
+  console.log(`[Playlist] Generating channel ${index + 1}/${channels.length}`);
+
+  const params: Record<string, string> = {
+    video_id: `${channel.id}`,
+  };
+
+  const streamUrl = await buildURL(ENDPOINTS.STREAM, params);
+  const streamData: Stream = (await (await fetch(streamUrl)).json()).data;
+
+  const { src } = streamData.bitrates[0];
+  const channelNumber = channel.index_numbers[0].number;
+
+  playlist +=
+    `#EXTINF:0 channel-id="${channel.id}" tvg-id="${channel.title}" tvg-logo="${channel.image.original}" tvg-chno="${channelNumber}" group-title="Megogo", ${channel.title}\n${src}\n`;
 }
+
+Deno.writeTextFileSync("generated/Megogo.m3u8", playlist);
+console.log("[Playlist] Done");
--- types.ts
+++ types.ts
@@ -29,3 +29,48 @@ interface ChannelImage {
   image_470x270: string;
   image_215x120: string;
 }
+
+export interface Stream {
+  video_id: number;
+  title: string;
+  hierarchy_titles: { TV: string };
+  src: string;
+  drm_type: string;
+  stream_type: string;
+  content_type: string;
+  audio_tracks: unknown;
+  subtitles: unknown;
+  bitrates: StreamBitrate[];
+  cdn_id: number;
+  advert_url: string;
+  allow_external_streaming: boolean;
+  start_session_url: string;
+  parental_control_required: boolean;
+  play_start_time: number;
+  watermark: string;
+  watermark_clickable_enabled: boolean;
+  show_best_quality_link: boolean;
+  share_link: string;
+  external_source: boolean;
+  channel_controls_restricted: boolean;
+  is_sport: boolean;
+  is_embed: boolean;
+  is_autoplay: boolean;
+  is_hierarchy: boolean;
+  is_wvdrm: boolean;
+  is_live: boolean;
+  is_tv: boolean;
+  is_3d: boolean;
+  is_uhd: boolean;
+  is_uhd_8k: boolean;
+  is_hdr: boolean;
+  is_favorite: boolean;
+}
+
+interface StreamBitrate {
+  index: number;
+  name: string;
+  bitrate: number;
+  src: string;
+  license_server: string;
+}
